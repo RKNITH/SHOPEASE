23827dc84bb691d682d4be2f9ae0002ce52e01f8
RKNITH <raviranjankofficial@gmail.com>
2024-11-21 16:40:31 +0530

    file changed

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -9,6 +9,8 @@ export default (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
+        secure: true,
+        sameSite: 'None'
     };
 
     res.status(statusCode).cookie("token", token, options).json({
